9e2f67d04c225a3910c54b18aab8456a274d0cd2
Roman <kenpachitox@gmail.com>
2021-09-27 16:04:30 +0300

    create certificate for server.dev

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,10 @@ WORKDIR /app
 COPY . .
 RUN mvn -B package
 
+FROM ubuntu
+WORKDIR /app
+COPY . .
+
 FROM tomcat:10-jdk16-openjdk-slim
 COPY --from=build /app/target/web-app-1.1.war $CATALINA_HOME/webapps/ROOT.war
 COPY apache-tomcat-10.0.11/conf/server.xml $CATALINA_HOME/conf
--- docker-entrypoint-initdb.d/01_data.sql
+++ docker-entrypoint-initdb.d/01_data.sql
@@ -1,6 +1,6 @@
 INSERT INTO users(id, username, password)
 VALUES (1, 'admin', '$argon2id$v=19$m=4096,t=3,p=1$CjM/3pdG9sYDzxGFDbesMA$HA1Fm8y23U9QucCFmq1hynPzyCsMfSNKpEqyZVif/og'),
-       (2, 'student', '$argon2id$v=19$m=4096,t=3,p=1$gCAC+zI1HRM5IRKJc8HhGw$RadiUVwSqt6m7icr6+Cp67Jh8Sc+YRs+zJ9xw2nYh6M');
+       (2, 'kenpachi', '$argon2id$v=19$m=4096,t=3,p=1$gCAC+zI1HRM5IRKJc8HhGw$RadiUVwSqt6m7icr6+Cp67Jh8Sc+YRs+zJ9xw2nYh6M');
 
 ALTER SEQUENCE users_id_seq RESTART WITH 3;
 
